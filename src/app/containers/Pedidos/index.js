157f9ea7e7691c8cb119d2bc8ef89958c98a4aef
Alan Vitor Paixão Almeida <vitorpaixaoa@MacBook-de-Alan.local>
2020-10-31 13:03:59 -0300

    Estrutura dos dados

--- src/app/containers/Pedidos/index.js
+++ src/app/containers/Pedidos/index.js
@@ -2,6 +2,7 @@ import React,{ Component } from 'react';
 
 import Titulo from '../../components/Texto/Titulo';
 import Pesquisa from '../../components/Inputs/Pesquisa'
+import moment from 'moment';
 
 class Pedidos extends Component {
 
@@ -13,7 +14,19 @@ class Pedidos extends Component {
 
 
     render(){
+        
         const {pesquisa} = this.state
+        
+        //dados
+        const dados = [
+            {
+                "Cliente": "Cliente 1",
+                "Valor Total": 89.90,
+                "Data": moment().toISOString(),
+                "Situação": "Aguardando Pagamento",
+                "botaoDeDetalhes":"/pedido/21391daas112221"
+            }
+        ]
         return (
         <div className="Pedidos">
                 <Titulo tipo="h1" titulo="Pedidos" />
@@ -23,7 +36,9 @@ class Pedidos extends Component {
                 placeholder={"Pesquise pelo nome do cliente."}
                 onChange={(ev)=> this.onChangePesquisa(ev)}/>    
                 <br/>
-                <Tabela />
+                <Tabela 
+                    cabecalho={["Cliente","Valor Total", "Data", "Situação"]}
+                    dados={dados} />
                 <Paginacao/>
 
         </div>)
