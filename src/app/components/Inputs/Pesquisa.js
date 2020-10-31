d4282206d466b769e09cdbccdddb0c6f963e5295
Alan Vitor Paixão Almeida <vitorpaixaoa@MacBook-de-Alan.local>
2020-10-31 12:57:50 -0300

    Pesquisa completa

---
+++ src/app/components/Inputs/Pesquisa.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Pesquisa = ({valor, onChange, placeholder}) =>(
+    <div className="Pesquisa">
+        <input value={valor} onChange={onChange} placeholder={placeholder}></input>
+
+    </div>
+)
+export default Pesiquisa;
--- src/app/containers/Pedidos/index.js
+++ src/app/containers/Pedidos/index.js
@@ -1,12 +1,31 @@
 import React,{ Component } from 'react';
 
 import Titulo from '../../components/Texto/Titulo';
+import Pesquisa from '../../components/Inputs/Pesquisa'
 
 class Pedidos extends Component {
+
+    state = {
+        pesquisa:""
+    }
+
+    onChangePesquisa= (ev) => this.setState({ pesquisa: ev.target.value })
+
+
     render(){
+        const {pesquisa} = this.state
         return (
         <div className="Pedidos">
-            <Titulo tipo="h1" titulo="Pedidos" />
+                <Titulo tipo="h1" titulo="Pedidos" />
+                <br/>
+                <Pesquisa 
+                valor ={pesquisa} 
+                placeholder={"Pesquise pelo nome do cliente."}
+                onChange={(ev)=> this.onChangePesquisa(ev)}/>    
+                <br/>
+                <Tabela />
+                <Paginacao/>
+
         </div>)
     }
 }
