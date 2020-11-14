e6b08138a223ef01622344e7d2a8aeb02becb638
Alan Vitor Paixão Almeida <vitorpaixaoa@MacBook-de-Alan.local>
2020-11-14 14:06:35 -0300

    parei na variação selecionada

--- src/app/components/Imagens/Bloco.js
+++ src/app/components/Imagens/Bloco.js
@@ -1,21 +1,27 @@
 import React from 'react';
 
+import Titulo from '../Texto/Titulo'
 class BlocoImagem extends React.Component {
     render(){
         const { handleSubmit, imagens, onRemove} = this.props
         return(
             <div className="Bloco-Imagem">
-                <div>
+            <Titulo tipo="h3" tipo="Imagens" />
+            <div>
+                <Titulo tipo="h3" titulo="Imagens" />
+            </div>
+                <div className="flex horizontal">
+                    <label><strong> Insira aqui uma nova imagem:&nbsp;</strong></label>
                     <input type="file" onChange={handleSubmit}/>
                 </div>
                 <div className="imagens-container">
                     {
                         imagens.map((src, idx) => (
                             <div
-                                className="imagem-container"
+                                className="imagem-container flex flex-center"
                                 style={{backgroundImage: `url("${src}")`}}
                                 key={idx}> 
-                                    <div className="imagem-remover" onClick={()=> onRemove(idx)}>
+                                    <div className="imagem-remover flex flex-center" onClick={()=> onRemove(idx)}>
                                         <span>{"-"}</span>
                                     </div>
                                 
--- src/app/containers/Produto/detalhesVariacoes/variacoes.js
+++ src/app/containers/Produto/detalhesVariacoes/variacoes.js
@@ -3,19 +3,20 @@ import Titulo from '../../../components/Texto/Titulo';
 
 class Variacoes extends Component {
     state={
+        variacaoSelecionada:"AKDHHH2KKK12",
         variacoes:[
             {nome: "P", id:"AKDHHH2KKK12"},
             {nome: "M", id:"AKSDDDSASKK12"}
         ]
     }
     render(){
-        const { variacoes } = this.state
+        const { variacoes, variacaoSelecionada} = this.state
         return (
             <div className="variacoes flex vertical flex-center">
                 <Titulo tipo="h2" titulo="Variações" />
                 {
                     variacoes.map((item, idx) => (
-                       <div className="variacao-item" key={idx}>
+                       <div className={`variacao-item ${variacaoSelecionada ? "variacao-selecionada" : ""}`} key={idx}>
                            <span>{item.nome}</span>
                        </div>
                     ))
