455e7087c6d5934d7e1abf20d97b1f5e0d57f9f2
vitorpaixaoa <vitorpaixaoa.loubi@gmail.com>
2020-11-23 12:49:42 -0300

    Detalhes do pedido confirmados

--- src/app/containers/Pedido/DetalhesDaEntrega.js
+++ src/app/containers/Pedido/DetalhesDaEntrega.js
@@ -1,45 +1,67 @@
-import React,{Component} from 'react'
-import Titulo from '../../components/Texto/Titulo'
-import ListaDinamica from '../../components/Listas/ListaDinamicaSimples'
-import InputValor  from '../../components/Inputs/InputValor'
+import React, { Component } from 'react';
 
-class DetalhesDaEntrega extends Component{
-    state={
-        status:[
-            "Preparando para envio",
-            "Entregue a transportadora",
-            "Em trânsito"
-        ],
-        codigoDeRastreamento: "PA123456BR"
+import Titulo from '../../components/Texto/Titulo';
+import ListaDinamica from '../../components/Listas/ListaDinamicaSimples';
+import InputValor from '../../components/Inputs/InputValor';
+
+import * as actions from '../../Actions/pedidos';
+import { connect } from 'react-redux';
+import AlertGeral from '../../components/Alert/Geral';
+
+class DetalhesDaEntrega extends Component {
+    state = {
+        aviso: null
+    }
+
+    cleanState(){
+        this.setState({ aviso: null })
     }
 
     onAddListaDinamica = (texto) => {
-        if(!texto) return false;
-        let {status} = this.state
-        status.push(texto)
-        this.setState({ status })
+        if(!texto) return this.setState({ aviso: { status: false, msg: "Preencha o campo para enviar um novo status" } });
+        this.setNovoStatus(texto, undefined);
     }
-    
 
-    handleSubmit =(value) => {
-        this.setState({ codigoDeRastreamento : value})
-        alert("SALVO!")
+    setNovoStatus = (status, codigoRastreamento) => {
+        this.cleanState();
+
+        const { pedido, usuario } = this.props;
+        if(!usuario || !pedido) return false;
+        this.props.setNovoStatusEntrega(
+            { status, codigoRastreamento },
+            pedido.pedido.entrega._id, pedido.pedido._id, usuario.loja,
+            (error) => {
+                if(error) this.setState({ aviso: false, msg: error.message });
+            }
+        )
+    }
+
+    handleSubmit = (value) => {
+        if(!value) return this.setState({ aviso: { status: false, msg: "Preencha o código de rastreamento corretamente" } });
+        this.setNovoStatus("Atualização no Cód. de Rastreamento", value);
     }
 
     render(){
-        const {status, codigoDeRastreamento} = this.state
+        
+        const { pedido } = this.props;
+        const { aviso } = this.state;
+        if(!pedido) return <div></div>;
+
+        const status = (pedido.registros || [])
+                        .reduce((all, item) => item.tipo === 'entrega' ? all.concat([item.situacao]) : all, []);
+        const { codigoRastreamento } = pedido.pedido.entrega;
         return (
             <div className="Detalhes-do-Entrega">
                 <Titulo tipo="h3" titulo="Entrega" />
-                <br/>
-                <label>Codigo de Rastreamento:</label>
+                <AlertGeral aviso={aviso} />
+                <br />
+                <label>Código de Rastreamento</label>
                 <InputValor
-                    value={codigoDeRastreamento}
-                    handleSubmit={(value)=> this.handleSubmit(value)}
-                    name={"codigoDeRastreamento"}
-                />
-                <br/>
-                <ListaDinamica 
+                    value={codigoRastreamento}
+                    handleSubmit={(value) => this.handleSubmit(value)}
+                    name={"codigoRastreamento"} />
+                <br />
+                <ListaDinamica
                     dados={status}
                     onAdd={this.onAddListaDinamica} />
             </div>
@@ -47,4 +69,9 @@ class DetalhesDaEntrega extends Component{
     }
 }
 
-export default DetalhesDaEntrega
+const mapStateToProps = state => ({
+    pedido: state.pedido.pedido,
+    usuario: state.auth.usuario
+})
+
+export default connect(mapStateToProps, actions)(DetalhesDaEntrega);
--- src/app/containers/Pedido/DetalhesDoPagamento.js
+++ src/app/containers/Pedido/DetalhesDoPagamento.js
@@ -1,29 +1,48 @@
-import React,{Component} from 'react'
-import Titulo from '../../components/Texto/Titulo'
-import ListaDinamica from '../../components/Listas/ListaDinamicaSimples'
+  
+import React, { Component } from 'react';
 
-class DetalhesDoPagamento extends Component{
-    state={
-        status:[
-            "Aguardando Pagamento",
-            "Pagamento Efetuado"
-        ]
+import Titulo from '../../components/Texto/Titulo';
+import ListaDinamica from '../../components/Listas/ListaDinamicaSimples';
+
+import * as actions from '../../Actions/pedidos';
+import { connect } from 'react-redux';
+import AlertGeral from '../../components/Alert/Geral';
+
+class DetalhesDoPagamento extends Component {
+    state = {
+        aviso: null
+    }
+
+    cleanState(){
+        this.setState({ aviso: null })
     }
 
     onAddListaDinamica = (texto) => {
-        if(!texto) return false;
-        let {status} = this.state
-        status.push(texto)
-        this.setState({ status })
+        this.cleanState();
+        if(!texto) return this.setState({ aviso: { status: false, msg: "Preencha o campo para enviar um novo status" } });
+        
+        const { pedido, usuario } = this.props;
+        this.props.setNovoStatusPagamento(texto, pedido.pedido.pagamento._id, pedido.pedido._id, usuario.loja, (error) => {
+            if(error) this.setState({ aviso: { status: false, msg: error.message } });
+        });
     }
 
     render(){
-        const {status} = this.state
+        
+        const { pedido } = this.props;
+        const { aviso } = this.state;
+
+        if(!pedido) return <div></div>;
+
+        const status = (pedido.registros || [])
+                        .reduce((all, item) => item.tipo === 'pagamento' ? all.concat([item.situacao]) : all, []);
+        
         return (
             <div className="Detalhes-do-Pagamento">
                 <Titulo tipo="h3" titulo="Pagamento" />
-                <br/>
-                <ListaDinamica 
+                <AlertGeral aviso={aviso} />
+                <br />
+                <ListaDinamica
                     dados={status}
                     onAdd={this.onAddListaDinamica} />
             </div>
@@ -31,4 +50,9 @@ class DetalhesDoPagamento extends Component{
     }
 }
 
-export default DetalhesDoPagamento
+const mapStateToProps = state => ({
+    pedido: state.pedido.pedido,
+    usuario: state.auth.usuario
+})
+
+export default connect(mapStateToProps, actions)(DetalhesDoPagamento);
