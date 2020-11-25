30b4fd5221e2d3424cfcdfaf5c69a4dd6d309407
vitorpaixaoa <vitorpaixaoa.loubi@gmail.com>
2020-11-24 21:34:51 -0300

    detalhes da categoria

--- src/app/containers/Categoria/detalhesCategoria.js
+++ src/app/containers/Categoria/detalhesCategoria.js
@@ -5,28 +5,88 @@ import InputValor from '../../components/Inputs/InputValor';
 import { TextoDados } from '../../components/Texto/Dados';
 import InputSelect from '../../components/Inputs/Select'
 
+import Voltar from '../../components/Links/Voltar'
+
+import { connect } from 'react-redux';
+import AlertGeral from '../../components/Alert/Geral';
+import * as actions from '../../Actions/categorias';
+
+
+ 
 class DetalhesCategoria extends Component {
 
-    state={
-        nome: "Categoria",
-        disponibilidade: "disponivel",
-        codigo: "categoria"
+
+    generateStateCategoria = (props) => ({
+            nome: props.categoria ? props.categoria.nome : "",
+            disponibilidade: props.categoria ? 
+                                ( props.categoria.disponibilidade || 
+                                    props.categora.disponibilidade === undefined  ) ? 
+                                    "disponivel" : "indisponivel" : "",
+            codigo: props.categora ? props.categoria.codigo : ""
+        });
+
+        constructor(props){
+            super();
+            this.state = {
+                ...this.generateStateCategoria(props),
+                erros: {},
+                aviso: null
+            }
+        }
+
+        componentWillUpdate(nextProps){
+            if(
+                ( !this.props.categoria && nextProps.categoria ) ||
+                ( 
+                    this.props.categoria && 
+                    nextProps.categoria && 
+                    this.props.categoria.updatedAt !== nextProps.categoria.updatedAt 
+                )
+            ) this.setState(this.generateStateCategoria(nextProps));
+        }
+
+
+    salvarCategoria(){
+        const { usuario, categoria } = this.props;
+        if( !usuario || !categoria ) return null;
+        if( !this.validade() ) return null;
+        console.log("oi mundo")
+
+        this.props.updateCategoria(this.state, categoria._id, usuario.loja, (error) => {
+            this.setState({
+                aviso: {
+                    status: !error,
+                    msg: error ? error.message : "Categoria Atualizar com sucesso. "
+                }
+            });
+        });
+    }
+    removerCategoria(){
+        const { usuario, categoria } = this.props;
+        if( !usuario || !categoria ) return null;
+
+        if(!window.confirm("Você realmente deseja remover essa categoria ?")) return;
+
+        this.props.removerCategoria( categoria._id, usuario.loja, (error) => {
+            if( error ) this.setState({ aviso: { status: false, msg:  error.message } });
+            else this.props.history.goBack();
+        });
     }
 
     renderCabecalho(){
-        const{nome } = this.state
+        const{ nome } = this.state;
         return(
             <div className="flex">
                 <div className="flex-1 flex">
-                    <Titulo tipo="h1" titulo={nome} />
+                    <Titulo tipo="h1" titulo={ nome } />
                 </div>
                 <div className="flex flex-end">
                     <ButtonSimples 
-                        onClick={()=> alert("Salvo")}
+                        onClick={()=> this.salvarCategoria() }
                         type="success"
                         label="Salvar" />
                     <ButtonSimples 
-                        onClick={()=> alert("Removido")}
+                        onClick={()=> this.removerCategoria() }
                         type="danger"
                         label="Remover" />
                 </div>
@@ -34,26 +94,42 @@ class DetalhesCategoria extends Component {
         )
     }
 
+
+    onChangeInput = ( field, value ) => this.setState({ [field]: value }, () => this.validate() );
+    
+    validate(){
+        const { nome, codigo } = this.state;
+        const erros = {};
+
+        if(!nome) erros.nome = "Preencha aqui com o nome da categoria";
+        if(!codigo) erros.codigo = "Preencha aqui com o código da categoria";
+        if(codigo && codigo.length < 4) erros.codigo = "Preencha com mais que 4 caracteres";
+        if(codigo && codigo.indexOf(" ") !== -1 ) erros.codigo = "Não coloque espacos no código";
+
+        this.setState({ erros });
+        return !( Object.keys(erros).length > 0 );
+    }
+
     renderDados(){
-        const{nome, disponibilidade, codigo } = this.state
+        const{nome, disponibilidade, codigo, erros } = this.state
         return(
             <div >
                    <TextoDados
                 chave="Codigo"
                 valor={(
                     <InputValor
-                        name="codigo" noStyle
+                        name="codigo" noStyle erro={erros.codigo}
                         value={codigo}
-                        handleSubmit ={(valor) => this.setState({ codigo: valor })} />
+                        handleSubmit ={(valor) => this.onChangeInput("codigo", valor)} />
             )}
          />
                     <TextoDados
                 chave="Nome"
                 valor={(
                     <InputValor
-                        name="nome" noStyle
+                        name="nome" noStyle erro={erros.nome}
                         value={nome}
-                        handleSubmit ={(valor) => this.setState({ nome: valor })} />
+                        handleSubmit ={(valor) => this.onChangeInput( "nome", valor )} />
             )}
          /><br/>
          <TextoDados 
@@ -79,6 +155,8 @@ class DetalhesCategoria extends Component {
     render(){
         return(
             <div className="Detalhes-Categoria">
+                <Voltar history={this.props.history} />
+                <AlertGeral aviso={this.state.aviso} />
                 {this.renderCabecalho() }
                 {this.renderDados()}
             </div>
@@ -87,4 +165,9 @@ class DetalhesCategoria extends Component {
     }
 }
 
-export default DetalhesCategoria
+const mapStateToProps = state => ({
+    categoria: state.categoria.categoria,
+    usuario: state.auth.usuario
+})
+
+export default connect(mapStateToProps, actions)(DetalhesCategoria)
